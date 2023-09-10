d774cf0a01f7a6266b99d8a1797398d3dbccbb66
Zhunity <poinseting@163.com>
2023-09-10 16:23:47 +0800

    物体渲染

--- ShaderTest/Assets/Pipeline/CameraRenderer.cs
+++ ShaderTest/Assets/Pipeline/CameraRenderer.cs
@@ -3,16 +3,47 @@ using UnityEngine.Rendering;
 
 public class CameraRenderer
 {
+	/// <summary>
+	/// We don't see anything yet because we also have to indicate which kind of shader passes are allowed. 
+	/// As we only support unlit shaders in this tutorial 
+	/// we have to fetch the shader tag ID for the SRPDefaultUnlit pass, 
+	/// which we can do once and cache it in a static field.
+	/// </summary>
+	static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+	const string bufferName = "Render Camera";
+
+		/// <summary>
+		/// It's as if we've written buffer.name = bufferName; 
+		/// as a separate statement after invoking the constructor. 
+		/// But when creating a new object, you can append a code block to the constructor's invocation. 
+		/// Then you can set the object's fields and properties in the block 
+		/// without having to reference the object instance explicitly. 
+		/// It makes explicit that the instances should only be used after those fields and properties have been set.
+		/// Besides that, it makes initialization possible where only a single statement
+		/// is allowed—for example a field initialization, 
+		/// which we're using here—without requiring constructors with many parameter variants.
+		/// 
+		/// Note that we omitted the empty parameter list of the constructor invocation, 
+		/// which is allowed when object initializer syntax is used.
+		/// </summary>
+		CommandBuffer buffer = new CommandBuffer
+	{
+		name = bufferName
+	};
 
 	ScriptableRenderContext context;
 
 	Camera camera;
+	CullingResults cullingResults;
 
 	public void Render(ScriptableRenderContext context, Camera camera)
 	{
 		this.context = context;
 		this.camera = camera;
-
+		if (!Cull())
+		{
+			return;
+		}
 		Setup();
 		DrawVisibleGeometry();
 		Submit();
@@ -28,6 +59,15 @@ public class CameraRenderer
 	void Setup()
 	{
 		context.SetupCameraProperties(camera);
+		// CommandBuffer.ClearRenderTarget requires at least three arguments.
+		// The first two indicate whether the depth and color data should be cleared,
+		// which is true for both. The third argument is the color used to clearing,
+		// for which we'll use Color.clear.
+		buffer.ClearRenderTarget(true, true, Color.red);    // 选中摄像机会变成后面的颜色
+		buffer.BeginSample(bufferName);
+		
+		ExecuteBuffer();
+		
 	}
 
 	/// <summary>
@@ -37,11 +77,40 @@ public class CameraRenderer
 	/// </summary>
 	void DrawVisibleGeometry()
 	{
+		var sortingSettings = new SortingSettings(camera)
+		{
+			criteria = SortingCriteria.CommonOpaque
+		};
+		var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+		var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+		context.DrawRenderers(
+			cullingResults, ref drawingSettings, ref filteringSettings
+		);
+
 		context.DrawSkybox(camera);
 	}
 
 	void Submit()
 	{
+		buffer.EndSample(bufferName);
+		ExecuteBuffer();
 		context.Submit();
+	}
+
+	void ExecuteBuffer()
+	{
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+
+	bool Cull()
+	{
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+		{
+			cullingResults = context.Cull(ref p);
+			return true;
+		}
+		return false;
 	}
 }
