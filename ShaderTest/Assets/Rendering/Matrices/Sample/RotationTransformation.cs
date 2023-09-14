70aa325fef800f15c5876f153ed71f2b5e69406a
lizhenhua.0769 <lizhenhua.0769@bytedance.com>
2023-09-14 17:00:28 +0800

    矩阵第一个实例

---
+++ ShaderTest/Assets/Rendering/Matrices/Sample/RotationTransformation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotationTransformation : Transformation {
+
+	public Vector3 rotation;
+
+	public override Vector3 Apply (Vector3 point) {
+		float radZ = rotation.z * Mathf.Deg2Rad;
+		float sinZ = Mathf.Sin(radZ);
+		float cosZ = Mathf.Cos(radZ);
+
+		point = new Vector3(
+			cosZ * point.x - sinZ * point.y,
+			sinZ * point.x + cosZ * point.y,
+			point.z
+		);
+
+		return point;
+	}
+}
